ed1e5c9bd3a863505fd3a9c45d025a6896153575
CodeMaster <CodeMaster@gmail>
2024-01-10 19:41:49 +0000

    minor changes

--- ProjectCode/BlazorApp1/BlazorApp1/Program.cs
+++ ProjectCode/BlazorApp1/BlazorApp1/Program.cs
@@ -1,4 +1,4 @@
-using BlazorApp1.Components;
+using BlazorApp1.Components.Pages;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,14 +15,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 // I want to redirect to login
-app.MapRazorComponents<App>()
+app.MapRazorComponents<Login>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
