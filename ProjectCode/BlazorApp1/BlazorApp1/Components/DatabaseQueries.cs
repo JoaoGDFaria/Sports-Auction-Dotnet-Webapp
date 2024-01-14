533d70d30af55c04d68c561a1a6431b06929e244
CodeMaster <CodeMaster@gmail>
2024-01-14 22:28:35 +0000

    Register data fields verification and insetion into database!

--- ProjectCode/BlazorApp1/BlazorApp1/Components/DatabaseQueries.cs
+++ ProjectCode/BlazorApp1/BlazorApp1/Components/DatabaseQueries.cs
@@ -32,7 +32,7 @@ public class DatabaseQueries{
 
     public async Task<List<Leilao>> GetAllAuctions()
     {
-        string sql = "SELECT * FROM ArtigoLeilao ORDER BY IdLeilao;";
+        string sql = "SELECT * FROM ArtigoLeilao";
 
         var parameters = new {};
         string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
@@ -42,62 +42,84 @@ public class DatabaseQueries{
         {
             leiloes = await _data.ExecuteQueryList<Leilao>(sql, parameters, connectionString);
         }
-
-
-        foreach (Leilao leilao in leiloes){
-            leilao.SetHighestBid(await GetHighestBid(leilao.GetIdLeilao()));
-        }
-
-
         return leiloes;
     }
 
-
-    public async Task<double> GetHighestBid(int auctionID)
-    {
-        string sql = "SELECT ValorLicitacao FROM Licitacao WHERE IdLeilao = @auctionID";
-
-        var parameters = new {auctionID};
-        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
-
-        List<double> bids = new List<double>();
-        if (connectionString != null)
-        {
-            bids = await _data.ExecuteQueryList<double>(sql, parameters, connectionString);
-        }
-
-        if (bids.Count == 0) return 0;
-        else return bids.Max();
-
-    }
-
-
-
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // USER QUERIES //////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    
     public async Task<int> RegisterUser(string firstName, string lastName, string email, string password, string address, string phoneNumber, string bin){
 
-    string sql = "INSERT INTO Utilizador (PrimeiroNome, UltimoNome, Email, PalavraPasse, Morada, NumeroTelemovel, NIB) "+ 
+        string sql = "INSERT INTO Utilizador (PrimeiroNome, UltimoNome, Email, PalavraPasse, Morada, NumeroTelemovel, NIB) "+ 
                 "VALUES (@PrimeiroNome, @UltimoNome, @Email, @PalavraPasse, @Morada, @NumeroTelemovel, @NIB)";
 
-    var parameters = new
-    {
-        @PrimeiroNome = firstName,
-        @UltimoNome = lastName,
-        @Email = email,
-        @PalavraPasse = password,
-        @Morada = address,
-        @NumeroTelemovel = phoneNumber,
-        @NIB = bin
-    };
+        var parameters = new
+        {
+            @PrimeiroNome = firstName,
+            @UltimoNome = lastName,
+            @Email = email,
+            @PalavraPasse = password,
+            @Morada = address,
+            @NumeroTelemovel = phoneNumber,
+            @NIB = bin
+        };
 
-    string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
+        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
 
-    if (connectionString != null)
-    {
-        await _data.ExecuteQuery<int>(sql, parameters, connectionString);
-    } 
+        if (connectionString != null)
+        {
+            await _data.ExecuteQuery<int>(sql, parameters, connectionString);
+        } 
 
-    return 1;
+        return 1;
     }
+
+    public async Task<bool> isThereNIB(string nib){
+        string sql = "SELECT NIB FROM Utilizador WHERE NIB = @nib";
+
+        var parameters = new { @nib = Convert.ToInt64(nib) };
+        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
+
+        long id = 0;
+        if (connectionString != null)
+        {
+            id = await _data.ExecuteQuery<long>(sql, parameters, connectionString);
+        }
+        return id!=0;
+    }
+
+    public async Task<bool> isThereEmail(string email){
+        string sql = "SELECT Email FROM Utilizador WHERE Email = @email";
+
+        var parameters = new { @email = email };
+        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
+
+        string resultEmail = string.Empty;
+        if (connectionString != null)
+        {
+            resultEmail = await _data.ExecuteQuery<string>(sql, parameters, connectionString);
+        }
+        return !string.IsNullOrEmpty(resultEmail);
+    }
+
+    public async Task<bool> isTherePhoneNumber(string phoneNumber){
+        string sql = "SELECT NumeroTelemovel FROM Utilizador WHERE NumeroTelemovel = @phoneNumber";
+
+        var parameters = new { @phoneNumber = Convert.ToInt64(phoneNumber) };
+        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
+
+        long id = 0;
+        if (connectionString != null)
+        {
+            id = await _data.ExecuteQuery<long>(sql, parameters, connectionString);
+        }
+        return id!=0;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    // ITEM QUERIES //////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public async Task<int> AddItem(string nomeLeilao, string imagemArtigo, string precoBaseLeilao, string nomeArtigo, string numeroAutenticacaoArtigo, string precoCompraAutomaticoLeilao, string nomeEquipaEventoArtigo, string tamanhoArtigo, string taxaMinimaIncrementoLeilao, string descricaoArtigo, string estadoArtigo, string dataUsoArtigo, string dataFinalizacaoLeilao, string idCategoria){
 
