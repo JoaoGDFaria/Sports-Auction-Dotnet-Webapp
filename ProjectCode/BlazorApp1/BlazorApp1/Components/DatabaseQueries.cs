7d1eeb5b74813d4777591bc8c2ad293eec05f570
hramos04 <116158805+hramos04@users.noreply.github.com>
2024-01-15 15:44:03 +0000

    converter imagem

--- ProjectCode/BlazorApp1/BlazorApp1/Components/DatabaseQueries.cs
+++ ProjectCode/BlazorApp1/BlazorApp1/Components/DatabaseQueries.cs
@@ -7,20 +7,22 @@ public class DatabaseQueries{
     private readonly ISqlDataAccess _data;
 
     
-    public DatabaseQueries(IConfiguration config, ISqlDataAccess data){
+    public DatabaseQueries(IConfiguration config, ISqlDataAccess data)
+    {
         _config = config;
         _data = data;
     }
 
-    public async Task<bool> GetLoginStatus(string email, string password){
-
+    public async Task<bool> GetLoginStatus(string email, string password)
+    {
         string sql = "SELECT NIB FROM Utilizador WHERE Email = @email AND PalavraPasse = @password";
 
         var parameters = new { email, password};
         string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
 
         long id = 0;
-        if (connectionString != null){
+        if (connectionString != null)
+        {
             id = await _data.ExecuteQuery<long>(sql, parameters, connectionString);
         }
         return id!=0;
@@ -28,8 +30,8 @@ public class DatabaseQueries{
 
 
 
-    public async Task<List<Leilao>> GetAllAuctions(){
-
+    public async Task<List<Leilao>> GetAllAuctions()
+    {
         string sql = "SELECT * FROM ArtigoLeilao";
 
         var parameters = new {};
@@ -41,103 +43,71 @@ public class DatabaseQueries{
             leiloes = await _data.ExecuteQueryList<Leilao>(sql, parameters, connectionString);
         }
 
-
         foreach (Leilao leilao in leiloes){
-            leilao.SetLicitacoes(await GetAllBids(leilao.GetIdLeilao()));
+            leilao.SetHighestBid(await GetHighestBid(leilao.GetIdLeilao()));
         }
+
+        await GetAllBids();
 
         return leiloes;
     }
 
 
-    public async Task<List<Licitacao>> GetAllBids(int idLeilao){
+       public async Task<double> GetHighestBid(int auctionID)
+    {
+        string sql = "SELECT ValorLicitacao FROM Licitacao WHERE IdLeilao = @auctionID";
 
-        string sql = "SELECT * FROM Licitacao WHERE IdLeilao = @idLeilao";
-
-        var parameters = new {idLeilao};
+        var parameters = new {auctionID};
         string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
 
-        List<Licitacao> bids = new List<Licitacao>();
-        if (connectionString != null){
-            bids = await _data.ExecuteQueryList<Licitacao>(sql, parameters, connectionString);
+        List<double> bids = new List<double>();
+        if (connectionString != null)
+        {
+            bids = await _data.ExecuteQueryList<double>(sql, parameters, connectionString);
         }
 
-        return bids;
-    }
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    //PARA A PAGINA AUCTIONS
-    public async Task<List<Leilao>> GetAllAuctionsWithoutUser(long idUser){
-
-        string sql = "SELECT * FROM ArtigoLeilao WHERE IdVendedor != @idUser";
-
-        var parameters = new {idUser};
-        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
-
-        List<Leilao> leiloes = new List<Leilao>();
-        if (connectionString != null){
-            leiloes = await _data.ExecuteQueryList<Leilao>(sql, parameters, connectionString);
-        }
-
-        foreach (Leilao leilao in leiloes){
-            leilao.SetLicitacoes(await GetAllBids(leilao.GetIdLeilao()));
-        }
-
-        return leiloes;
-    }
-
-
-    //PARA A PAGINA MYAUCTIONS
-    public async Task<List<Leilao>> GetAllAuctionsOfUser(long idUser){
-
-        string sql = "SELECT * FROM ArtigoLeilao WHERE IdVendedor = @idUser";
-
-        var parameters = new {idUser};
-        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
-
-        List<Leilao> leiloes = new List<Leilao>();
-        if (connectionString != null){
-            leiloes = await _data.ExecuteQueryList<Leilao>(sql, parameters, connectionString);
-        }
-
-        foreach (Leilao leilao in leiloes){
-            leilao.SetLicitacoes(await GetAllBids(leilao.GetIdLeilao()));
-        }
-
-        return leiloes;
+        if (bids.Count == 0) return 0;
+        else return bids.Max();
 
     }
 
 
-    public async Task<List<Leilao>> GetAllAuctionsWonByUser(long idUser){
+        public async Task<double> GetAllBids(){
 
-        string sql = "SELECT * FROM ArtigoLeilao WHERE IdVendedor = @idUser AND EstadoLeilao = 'Terminado'";
+            string sql = "SELECT * FROM Licitacao";
 
-        var parameters = new {idUser};
-        string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
+            var parameters = new {};
+            string connectionString = _config.GetConnectionString("DefaultConnection") ?? string.Empty;
 
-        List<Leilao> leiloes = new List<Leilao>();
-        if (connectionString != null){
-            leiloes = await _data.ExecuteQueryList<Leilao>(sql, parameters, connectionString);
+            List<Licitacao> bids = new List<Licitacao>();
+            if (connectionString != null)
+            {
+                bids = await _data.ExecuteQueryList<Licitacao>(sql, parameters, connectionString);
+            }
+
+            foreach (Licitacao licitacao in bids){
+                Console.WriteLine(licitacao.ToString());
+            }
+
+            return 0;
+
         }
 
-        foreach (Leilao leilao in leiloes){
-            leilao.SetLicitacoes(await GetAllBids(leilao.GetIdLeilao()));
-        }
-
-        return leiloes;
-
-    }
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+ 
+
+
+
+
+
+
+
+
+
 
 
 
@@ -221,10 +191,11 @@ public class DatabaseQueries{
     // ITEM QUERIES //////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public async Task<int> AddItem(string nomeLeilao, string imagemArtigo, string precoBaseLeilao, string nomeArtigo, string numeroAutenticacaoArtigo, string precoCompraAutomaticoLeilao, string nomeEquipaEventoArtigo, string tamanhoArtigo, string taxaMinimaIncrementoLeilao, string descricaoArtigo, string estadoArtigo, string dataUsoArtigo, string dataFinalizacaoLeilao, string idCategoria){
+    public async Task<int> AddItem(string nomeLeilao, byte[] imagemArtigo, string precoBaseLeilao, string nomeArtigo, string numeroAutenticacaoArtigo, string precoCompraAutomaticoLeilao, string nomeEquipaEventoArtigo, string tamanhoArtigo, string taxaMinimaIncrementoLeilao, string descricaoArtigo, string estadoArtigo, string dataUsoArtigo, string dataFinalizacaoLeilao){
 
     string estadoLeilao = "a decorrer";
     string idVendedor = "12324765";
+    string idCategoria = "134";
 
     string sql = "INSERT INTO ArtigoLeilao (NomeLeilao, ImagemArtigo, PrecoBaseLeilao, NomeArtigo, NumeroAutenticacaoArtigo, PrecoCompraAutomaticoLeilao, NomeEquipaEventoArtigo, TamanhoArtigo, TaxaMinimaIncrementoLeilao, DescricaoArtigo, EstadoArtigo, DataUsoArtigo, DataFinalizacaoLeilao, IdCategoria, IdVendedor, EstadoLeilao)" + 
                 "VALUES (@NomeLeilao, @ImagemArtigo, @PrecoBaseLeilao, @NomeArtigo, @NumeroAutenticacaoArtigo, @PrecoCompraAutomaticoLeilao, @NomeEquipaEventoArtigo, @TamanhoArtigo, @TaxaMinimaIncrementoLeilao, @DescricaoArtigo, @EstadoArtigo, @DataUsoArtigo, @DataFinalizacaoLeilao, @IdCategoria, @IdVendedor, @EstadoLeilao)";
